eb8b6ab7a505743f5f14b415df84892c79993584
uno0uno <anderson.electronico@gmail.com>
2023-04-15 22:28:59 -0500

    fix bug atlas

--- app/config/db_class.py
+++ app/config/db_class.py
@@ -30,6 +30,7 @@ from app.config.config import END_POINT_INFURA
 
 client = MongoClient(settings.URL_CLUSTER_MONGO_DB, tlsCAFile=certifi.where())
 
+
 class Validator(object):
     def __init__(self) -> None:
         pass
